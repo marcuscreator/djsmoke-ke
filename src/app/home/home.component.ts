fb46178d1a94481b3fa193ab5631669326ec51ad
marcuscreator <kariukimacharia71@gmail.com>
2024-10-29 16:06:10 +0300

    create hero section component and edit images and headingsEverything up-to-date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
