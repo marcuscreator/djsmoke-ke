53c92ce5accd2ef409d2ed27b55e191fbf38015e
marcuscreator <kariukimacharia71@gmail.com>
2024-10-29 22:41:15 +0300

    clean up code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { AboutusSectionComponent } from '../aboutus-section/aboutus-section.component';
 import { MainServicesComponent } from '../main-services/main-services.component';
+import { VideoSectionComponent } from '../video-section/video-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSectionComponent, AboutusSectionComponent, MainServicesComponent],
+  imports: [
+    HeroSectionComponent,
+    AboutusSectionComponent,
+    MainServicesComponent,
+    VideoSectionComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
